4fc0495d6a206cb540811ce263d07c24ebadc8c6
ascii274 <ascii274@gmail.com>
2018-01-03 10:16:06 +0100

    mi primer commit

---
+++ app/src/main/java/ambo/botonabrirventana/MainActivity.java
@@ -0,0 +1,38 @@
+package ambo.botonabrirventana;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.btnGoNexPage);
+        button.setOnClickListener(this);
+
+
+    }
+
+    public void onClick(View v){
+
+        switch (v.getId()){
+            case R.id.btnGoNexPage:
+                Intent intent=new Intent(this, Main2Activity.class);
+                startActivity(intent);
+                break;
+
+            default:
+                break;
+        }
+
+
+    }
+
+}
